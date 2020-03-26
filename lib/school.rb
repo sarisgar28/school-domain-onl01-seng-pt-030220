4702617ad5d7bd1f3a9e074c0216b3536204be34
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-26 02:31:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  
+  def initialize(school_name)
+    @school_name = school_name 
+    @roster = { }
+  end 
+ 
+  def add_student(name,grade)
+    if @roster[grade]
+      @roster[grade] << [name]
+    else
+      @roster[grade] = [name]
+    end 
+  end 
+  
+    
+    def sort(grade,name)
+      @roster.each do |grade,name|
+        @roster[grade] = name.sort
+      end 
+    end   
+  end 
